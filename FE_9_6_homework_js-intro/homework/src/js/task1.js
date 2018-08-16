bcc6c384a36cea91b3d3f1ba3dab9fad53e2810b
Ruslan Nersezashvili <kick09111@gmail.com>
2018-08-16 19:28:27 +0300

    Add S-Homework

--- FE_9_6_homework_js-intro/homework/src/js/task1.js
+++ FE_9_6_homework_js-intro/homework/src/js/task1.js
@@ -1,21 +1,21 @@
-const price=parseFloat(prompt('Amount of money: ', ''));
-const discount=parseFloat(prompt('Your discount: ', ''));
-let output;
+let price = parseFloat(prompt('Amount of money: ', ''));
+let discount = parseFloat(prompt('Your discount: ', ''));
+let output = 0;
 
 function isValidNumber(num) {
-	return !isNaN(num) && num>=0;
+	return !isNaN(num) && num >= 0;
 }
 
 function rounding(num) {
-	return Math.round(num*100)/100;
+	return Math.round(num * 100) / 100;
 }
 
-if(isValidNumber(price) && isValidNumber(discount) && discount<=100) {
-	const d_price=price*(100-discount)/100;
-	const saved=price-d_price;
-	output='Price without discount: '+rounding(price)+'\n'+'Discount: '+rounding(discount)+'%'
-	+'\n'+'Price with discount: '+rounding(d_price)+'\n'+'Saved: '+rounding(saved);
+if(isValidNumber(price) && isValidNumber(discount) && discount <= 100) {
+	let d_price = price * (100 - discount) / 100;
+	let saved = price - d_price;
+	output = 'Price without discount: ' + rounding(price) + '\n' + 'Discount: ' + rounding(discount) + '%'
+	+ '\n' + 'Price with discount: ' + rounding(d_price) + '\n' + 'Saved: ' + rounding(saved);
 } else {
-	output='Invalid data';
+	output = 'Invalid data';
 }
 console.log(output);
--- FE_9_6_homework_js-intro/homework/src/js/task2.js
+++ FE_9_6_homework_js-intro/homework/src/js/task2.js
@@ -1,37 +1,37 @@
-const side_A=parseFloat(prompt('Input side A: ',''));
-const side_B=parseFloat(prompt('Input side B: ',''));
-const angle=parseFloat(prompt('Input angle: ',''));
-const max_Angle=180;
-const convert=Math.PI / max_Angle;
-let output;
+let side_A = parseFloat(prompt('Input side A: ',''));
+let side_B = parseFloat(prompt('Input side B: ',''));
+let angle = parseFloat(prompt('Input angle: ',''));
+let max_Angle = 180;
+let convert = Math.PI / max_Angle;
+let output = 0;
 
 function isValidNumber(num) {
-	return !isNaN(num) && num>=0;
+	return !isNaN(num) && num >= 0;
 }
 
 function rounding(num) {
-	return Math.round(num*100)/100;
+	return Math.round(num * 100) / 100;
 }
 
 function findSideC(side1,side2,ang) {
- return Math.sqrt(Math.pow(side1,2)+Math.pow(side2,2)-2*side1*side2*Math.cos(ang*convert));
+	return Math.sqrt(Math.pow(side1,2) + Math.pow(side2,2) - 2 * side1 * side2 * Math.cos(ang * convert));
 }
 
 function findSquare(side1,side2,ang) {
- return side1*side2*Math.sin(ang*convert)/2;
+	return side1 * side2 * Math.sin(ang * convert) / 2;
 }
 
 function findPerimeter(side1,side2,side3) {
- return side1+side2+side3;
+	return side1 + side2 + side3;
 }
 
-if(isValidNumber(side_A) && isValidNumber(side_B) && isValidNumber(angle) && angle<=max_Angle) {
-	const side_C=findSideC(side_A,side_B,angle);
-	const square=findSquare(side_A,side_B,angle);
-	const perimeter=findPerimeter(side_A,side_B,side_C);
-	output='c length: '+rounding(side_C)+'\n'+'Triangle square: '+rounding(square)+
-	'\n'+'Triangle perimeter: '+rounding(perimeter)+'\n';
+if(isValidNumber(side_A) && isValidNumber(side_B) && isValidNumber(angle) && angle <= max_Angle) {
+	let side_C = findSideC(side_A,side_B,angle);
+	let square = findSquare(side_A,side_B,angle);
+	let perimeter = findPerimeter(side_A,side_B,side_C);
+	output = 'c length: ' + rounding(side_C) + '\n' + 'Triangle square: ' + rounding(square) +
+	'\n' + 'Triangle perimeter: ' + rounding(perimeter) + '\n';
 } else {
-	output='Invalid data';
+	output = 'Invalid data';
 }
 console.log(output);
